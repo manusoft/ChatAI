96ab8c1335e4ed718b1569e2bfa27a9afa7e5cbc
manoj@home <alconsoft@gmail.com>
2023-05-06 01:32:29 +0400

    Add project files.

---
+++ ChatAI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatAI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ChatAI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ChatAI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
